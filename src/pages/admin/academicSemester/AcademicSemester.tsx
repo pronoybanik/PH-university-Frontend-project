36386e2330f336bd8f36a8fcc97357bfdb30648d
PRONOY <pronoybanik82@gmail.com>
2025-01-15 19:06:02 +0600

    set academic fillter

--- src/pages/admin/academicSemester/AcademicSemester.tsx
+++ src/pages/admin/academicSemester/AcademicSemester.tsx
@@ -1,11 +1,124 @@
-import React from "react";
+import { Button, Table, TableColumnsType, TableProps } from "antd";
+import { TAcademicSemester } from "../../../types/academicSemesterManagement.type";
+import { TQueryParam } from "../../../types/global";
+import { useState } from "react";
 import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicmanagementApi";
 
-const AcademicSemester = () => {
-  const { data } = useGetAllSemestersQuery(undefined);
-  console.log(data);
+export type TTableData = Pick<
+  TAcademicSemester,
+  "name" | "year" | "startMonth" | "endMonth"
+>;
 
-  return <div>academic Semester semester</div>;
+const AcademicSemester = () => {
+  const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+  const {
+    data: semesterData,
+    isLoading,
+    isFetching,
+  } = useGetAllSemestersQuery(params);
+
+  console.log({ isLoading, isFetching });
+
+  const tableData = semesterData?.data?.result.map(
+    ({ _id, name, startMonth, endMonth, year }) => ({
+      key: _id,
+      name,
+      startMonth,
+      endMonth,
+      year,
+    })
+  );
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      key: "name",
+      dataIndex: "name",
+      filters: [
+        {
+          text: "Autumn",
+          value: "Autumn",
+        },
+        {
+          text: "Fall",
+          value: "Fall",
+        },
+        {
+          text: "Summer",
+          value: "Summer",
+        },
+      ],
+    },
+    {
+      title: "Year",
+      key: "year",
+      dataIndex: "year",
+      filters: [
+        {
+          text: "2024",
+          value: "2024",
+        },
+        {
+          text: "2025",
+          value: "2025",
+        },
+        {
+          text: "2026",
+          value: "2026",
+        },
+      ],
+    },
+    {
+      title: "Start Month",
+      key: "startMonth",
+      dataIndex: "startMonth",
+    },
+    {
+      title: "End Month",
+      key: "endMonth",
+      dataIndex: "endMonth",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: () => {
+        return (
+          <div>
+            <Button>Update</Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  const onChange: TableProps<TTableData>["onChange"] = (
+    _pagination,
+    filters,
+    _sorter,
+    extra
+  ) => {
+    if (extra.action === "filter") {
+      const queryParams: TQueryParam[] = [];
+
+      filters.name?.forEach((item) =>
+        queryParams.push({ name: "name", value: item })
+      );
+
+      filters.year?.forEach((item) =>
+        queryParams.push({ name: "year", value: item })
+      );
+      setParams(queryParams);
+    }
+  };
+
+  return (
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+    />
+  );
 };
 
 export default AcademicSemester;
--- src/redux/features/admin/academicmanagementApi.ts
+++ src/redux/features/admin/academicmanagementApi.ts
@@ -1,12 +1,32 @@
+import { TAcademicSemester } from "../../../types/academicSemesterManagement.type";
+import { TQueryParam, TResponseRedux } from "../../../types/global";
 import baseApi from "../../api/baseApi";
 
 const academicManagementApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
         getAllSemesters: builder.query({
-            query: () => ({
-                url: "/academic-semesters",
-                method: "GET",
-            }),
+            query: (args) => {
+                console.log(args);
+                const params = new URLSearchParams();
+
+                if (args) {
+                    args.forEach((item: TQueryParam) => {
+                        params.append(item.name, item.value as string);
+                    });
+                }
+
+                return {
+                    url: '/academic-semesters',
+                    method: 'GET',
+                    params: params,
+                };
+            },
+            transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
+                return {
+                    data: response.data,
+                    meta: response.meta,
+                };
+            },
         }),
         addAcademicSemesters: builder.mutation({
             query: (data) => ({
